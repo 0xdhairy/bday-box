c4e544443bec271cf785010bd755a0824029b3af
Dhairy Srivastava <dhairysrivastava5@gmail.com>
2025-07-13 19:19:43 +0530

    Complete

--- bday_box_esp32.ino
+++ bday_box_esp32.ino
@@ -6,7 +6,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Servo myservo;
+Servo lidServo;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int servoPin = 13;
@@ -14,7 +14,7 @@ int buttonPin = 15;
 int buzzerPin = 4;
 int startButtonPin = 5;
 bool started = false;
-int pos = 0;
+int pos = 180;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
@@ -26,56 +26,51 @@ void setup() {
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    
-  myservo.attach(servoPin, 500, 2400);
+  lidServo.setPeriodHertz(50);
+  lidServo.attach(servoPin, 500, 2400);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(16, 25);
+  display.println("Press the button ");
+  display.println("   for a suprise!");
   display.display();  // start blank
 }
 
 void loop() {
-  
-  
-  if (digitalRead(startButtonPin) == LOW) {
-    for (int i = 0; i < 100; i+= 1 ) {
-digitalWrite(buzzerPin, HIGH);
-  delay(50);
-  digitalWrite(buzzerPin, LOW);
-  delay(50);
-  }
-  }
+
+
+
   if (!started && digitalRead(buttonPin) == LOW) {
-    
-    delay(50); // debounce
+    display.clearDisplay();
+    display.display();
+    delay(50);  // debounce
     if (digitalRead(buttonPin) == LOW) {
       started = true;
-      digitalWrite(buzzerPin, HIGH); // turn buzzer ON
+      digitalWrite(buzzerPin, HIGH);  // turn buzzer ON
       delay(2000);                    // short buzz
-      digitalWrite(buzzerPin, LOW);  // turn buzzer OFF
+      digitalWrite(buzzerPin, LOW);   // turn buzzer OFF
 
       // Show message
       display.clearDisplay();
-      display.setTextSize(2);
+      display.setTextSize(1);
       display.setTextColor(SSD1306_WHITE);
-      display.setCursor(20, 25);
-      display.println("Hello!");
+      display.setCursor(16, 25);
+      display.println("Happy Bday ____!");
+      display.println("   - From Dhairy");
       display.display();
-
-      // Move servo once
-      for (pos = 0; pos <= 180; pos += 1) {
-        myservo.write(pos);
-        delay(15);
+      for (int pos = 180; pos >= 110; pos--) {
+        lidServo.write(pos);  // slowly opens the hinge
+        delay(5);
       }
-
     }
 
     delay(10000);
-    // Stop everything
+    lidServo.write(180);
     display.clearDisplay();
     display.display();
-    myservo.write(0);
-    myservo.detach(); // stops signal yeah
     started = false;
   }
 }
